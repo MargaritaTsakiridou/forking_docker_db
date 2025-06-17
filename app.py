db9dc014c3297f9919a86d9e22cc6c5f94b3a780
tomkdefra <tom.kirkpatrick@defra.gov.uk>
2025-06-17 23:18:53 +0100

    app now gets onebenthic data

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
+import requests
 import pandas as pd
 
-df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
-print("✅ DataFrame created:\n", df)
+url = "https://rconnect.cefas.co.uk/onebenthic_api_1/API-1_with_filters"
+
+params = [
+    ("in_year", 2020),
+    ("in_year", 2021),
+    ("in_sieve_size_mm", 1),
+    ("in_sieve_size_mm", 5),
+]
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    df = pd.DataFrame(response.json())
+    display(df.head(25))
+    
+else:
+    print(f"Request failed with status code: {response.status_code}")
+    print(response.text)
